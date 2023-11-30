f434c8f90b2bb5546f2b65cc8e29beef6ded56c9
StavrosVyzirakis <151865315+StavrosVyzirakis@users.noreply.github.com>
2023-11-30 19:44:15 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,24 @@
-
+        def calculate_sin(self):
+                self.number= int(self.current_calculation)
+                self.entry_box.delete(0,tk.END)
+                self.entry_box.insert(tk.END,m.sin(self.number))
+        def calculate_cos(self):
+                self.number=int(self.current_calculation)
+                self.entry_box.delete(0,tk.END)
+                self.entry_box.insert(tk.END,m.cos(self.number))
+        def calculate_tan(self):
+                self.number=int(self.current_calculation)
+                self.entry_box.delete(0,tk.END)
+                self.entry_box.insert(tk.END,m.tan(self.number))
+        def calculate_sinh(self):
+                self.number=int(self.current_calculation)
+                self.entry_box.delete(0,tk.END)
+                self.entry_box.insert(tk.END,m.sinh(self.number))        
+        def calculate_cosh(self):
+                self.number=int(self.current_calculation)
+                self.entry_box.delete(0,tk.END)
+                self.entry_box.insert(tk.END,m.cosh(self.number))
+        def calculate_tanh(self):
+                self.number=int(self.current_calculation)
+                self.entry_box.delete(0,tk.END)
+                self.entry_box.insert(tk.END,m.tanh(self.number))
